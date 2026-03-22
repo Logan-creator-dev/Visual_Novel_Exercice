c3b37e8bcb9b314d15ab015155dc59b2cc16c42b
logan <logan.villaret@gmail.com>
2026-03-22 19:47:45 +0100

    inventaire fonctionnel

--- Assets/Scriptes/ThumbnailControler.cs
+++ Assets/Scriptes/ThumbnailControler.cs
@@ -1,48 +1,70 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEditor;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ThumbnailController : MonoBehaviour {
-    
+public class ThumbnailController : MonoBehaviour
+{
+
     [SerializeField] private ThumbnailData _firstThumbnail;
     [SerializeField] private GameObject _buttonPrefab;
     [SerializeField] private Transform _itemPanelTransform;
-    
-    
+
+
     [SerializeField] private Image _thumbnail;
     [SerializeField] private TMP_Text _description;
     [SerializeField] private Transform _choicePanelTransform;
 
     public List<ItemData> Inventory = new();
 
-    private void Start() {
+    private void Start()
+    {
         DisplayThumbnail(_firstThumbnail);
     }
 
-    
-    private void DisplayThumbnail(ThumbnailData data) {
+
+    private void DisplayThumbnail(ThumbnailData data)
+    {
         _thumbnail.sprite = data.ThumbnailImage;
         _description.text = data.Description;
-        
+
+         if (data.GivenItem != null)
+         {
+             AddItem(data.GivenItem);
+         } 
+
         // Clear choices
-        foreach (Transform child in _choicePanelTransform) {
+        foreach (Transform child in _choicePanelTransform)
+        {
             Destroy(child.gameObject);
         }
-        
+
         // Instantiate choices
-        foreach (ChoiceData choiceData in data.ChoiceData) {
-            GameObject instantiate = Instantiate(_buttonPrefab, _choicePanelTransform);
-            instantiate.GetComponentInChildren<TextMeshProUGUI>().text = choiceData.Choice;
-            instantiate.GetComponent<Button>().onClick.AddListener(() => {
-                DisplayThumbnail(choiceData.LinkedThumbnail);
-            });
+        foreach (ChoiceData choiceData in data.ChoiceData)
+        {
+             if (choiceData.NeededItem != null && !Inventory.Contains(choiceData.NeededItem))
+             {
+                 continue;
+             } 
+                
+             GameObject instantiate = Instantiate(_buttonPrefab, _choicePanelTransform);
+             instantiate.GetComponentInChildren<TextMeshProUGUI>().text = choiceData.Choice;
+             instantiate.GetComponent<Button>().onClick.AddListener(() =>
+             {
+                 DisplayThumbnail(choiceData.LinkedThumbnail);
+             });
         }
     }
 
     public void AddItem(ItemData GivenItem)
     {
-        Inventory.Add(GivenItem);
+        if (!Inventory.Contains(GivenItem))
+        {
+            Inventory.Add(GivenItem);
+        }
     }
-}
+}  
+   
+  
